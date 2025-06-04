fdf81dd52e3b03aaf7f87061eb77294269ac4f1b
Dixadon <thenixmin@proton.me>
2025-06-04 14:05:50 -0400

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,4 +14,16 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  res.json({ message: 'Login successful', userId: user._id });
+});
+
+
 module.exports = router;
