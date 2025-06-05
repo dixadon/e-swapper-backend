cf4c2de8ec79a29f3e1d27e6fc24e409df6b452d
Dixadon <thenixmin@proton.me>
2025-06-05 11:41:02 -0400

    Add files via upload

---
+++ models/Item.js
@@ -0,0 +1,1 @@
+const mongoose=require('mongoose');const schema=new mongoose.Schema({title:String,description:String,owner:String});module.exports=mongoose.model('Item',schema);
---
+++ models/Message.js
@@ -0,0 +1,1 @@
+const mongoose=require('mongoose');const schema=new mongoose.Schema({from:String,to:String,content:String,timestamp:{type:Date,default:Date.now}});module.exports=mongoose.model('Message',schema);
