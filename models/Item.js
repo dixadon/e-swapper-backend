20beae6f62c453fa3580e8d36026e2b1b91e778b
Dixadon <thenixmin@proton.me>
2025-06-05 11:37:54 -0400

    Delete models directory

--- models/Item.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('Item', new mongoose.Schema({
-  title: String,
-  description: String,
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-}));
