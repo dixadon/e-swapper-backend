01c1e44ebcf75645d3757af93cef08751f5a53d2
Dixadon <thenixmin@proton.me>
2025-06-08 11:29:00 -0400

    Delete routes directory

--- routes/items.js
+++
@@ -1,4 +0,0 @@
-const express=require('express'),router=express.Router(),Item=require('../models/Item');
-router.get('/',async(_,res)=>{res.json(await Item.find())});
-router.post('/',async(req,res)=>{const item=new Item(req.body);await item.save();res.json({message:'Item listed'});});
-module.exports=router;
--- routes/users.js
+++
@@ -1,4 +0,0 @@
-const express=require('express'),router=express.Router(),User=require('../models/User'),jwt=require('jsonwebtoken');
-router.post('/register',async(req,res)=>{const user=new User(req.body);await user.save();res.json({message:'User created'});});
-router.post('/login',async(req,res)=>{const u=await User.findOne({username:req.body.username});if(u&&u.password===req.body.password){const token=jwt.sign({id:u._id},process.env.JWT_SECRET);res.json({token});}else{res.status(401).json({message:'Invalid'});}});
-module.exports=router;
