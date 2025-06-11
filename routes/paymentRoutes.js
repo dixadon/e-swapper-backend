a288580952638c736199deabaa28ea7d2b5e4f83
Dixadon <thenixmin@proton.me>
2025-06-11 14:03:39 -0400

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')('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'); // Replace with your secret key
+const stripe = require('stripe')('process.env.STRIPE_SECRET_KEY'); // Replace with your secret key
 const User = require('../models/User'); // Adjust path as needed
 
 router.post('/create-checkout-session', async (req, res) => {
